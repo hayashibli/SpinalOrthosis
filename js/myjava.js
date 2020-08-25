b45b3cee14fd006e62d54359d3100d17e086e680
hayashibli <haya98shibli@gmail.com>
2020-08-25 19:17:12 +0300

    added a js folder

---
+++ js/myjava.js
@@ -0,0 +1,10 @@
+var condition = prompt('Do you have a spinal deformity or injury?')
+var yes='yes'
+var no='no'
+
+if (condition == yes) { alert('You will find your solution with us')
+    
+} else if(condition == no)  {alert('Check our website')
+    
+}
+confirm("Press ok to continue, please!");
