3e00c041082be622f624fc366c03b274217e559d
hayashibli <haya98shibli@gmail.com>
2020-08-26 14:36:48 +0300

    modify myjava file

--- js/myjava.js
+++ js/myjava.js
@@ -1,10 +1,21 @@
-var condition = prompt('Do you have a spinal deformity or injury?')
-var yes='yes'
-var no='no'
 
-if (condition == yes) { alert('You will find your solution with us')
+
+
+function Case(){
+    //input
+    confirm("Press ok to continue, please!");
+    var condition = prompt('Do you have a spinal deformity or injury?')
+    var yes='yes'
+    var no='no'
+    
+//processing
+    if (condition == yes) { alert('You will find your solution with us')
     
 } else if(condition == no)  {alert('Check our website')
+
     
-}
-confirm("Press ok to continue, please!");
+//output
+return (condition);
+
+}; }
+
