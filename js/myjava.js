fddc696513d3946eda844eefd9383857a899e5a0
hayashibli <haya98shibli@gmail.com>
2020-08-27 11:27:27 +0300

    modify myjava.js

--- js/myjava.js
+++ js/myjava.js
@@ -2,8 +2,9 @@ function Case() {
     //input
     confirm("Press ok to continue, please!");
     var condition = prompt('Do you have a spinal deformity or injury?')
-    var x = 'yes'
-    var y = 'no'
+    //var x = 'yes'
+    //var y = 'no'
+    var patient = "";
     
 
     //processing
@@ -19,10 +20,14 @@ function Case() {
 
         }
 
+        var numberofdevices = prompt('how many devices you need?');
+        for (var count=0; count< numberofdevices; count++) {
+            if (condition == "yes") {
+                patient = patient + 'check the first link'}
+
+
         //output
         return (condition);
 
     };
-
-
-
+        }
