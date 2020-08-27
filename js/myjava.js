63fe0e37c1da2db9dcca2b8318707ff997befa3a
hayashibli <haya98shibli@gmail.com>
2020-08-27 13:13:56 +0300

    modify js file

--- js/myjava.js
+++ js/myjava.js
@@ -2,28 +2,22 @@ function Case() {
     //input
     confirm("Press ok to continue, please!");
     var condition = prompt('Do you have a spinal deformity or injury?')
-    //var x = 'yes'
-    //var y = 'no'
-    var patient = "";
-    
+    var x = 'yes'
+    var y= 'no'
 
     //processing
     while (condition !== 'yes' && condition !== 'no') {
         condition = prompt('Do you have a spinal deformity or injury?');
     }
 
+
     if (condition == x) {
         alert('You will find your solution with us')
 
     } else if (condition == y) {
         alert('Check our website')
-
-        }
-
-    
-
+    }
         //output
         return (condition);
 
     };
-        }
